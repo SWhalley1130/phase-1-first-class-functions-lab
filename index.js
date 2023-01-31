6e7ebd019c9f4853341446918f78d46c70bad172
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-31 15:55:58 -0700

    Completes lab

--- index.js
+++ index.js
@@ -5,17 +5,33 @@ const returnLastTwoDrivers=p1=>p1.slice(2);
 
 const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier(num)
-{
-    return (num)=>num*num;
-}
-
-// const fareDoubler = fare =>
+// below function gets test to pass but cannot be used on next two functions
+// function createFareMultiplier(num)
 // {
-//     const doubledFare = createFareMultiplier(fare);
-//     return doubledFare; 
+//     return (num)=>num*num;
 // }
 
 
+// below function was created using solutions sheet as a guide  
+function createFareMultiplier(num)
+{
+    return function (p1) 
+    {
+        return p1*num;
+    }
+}
 
+const fareDoubler=createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3);
+
+
+
+// below two function get tests to pass but do not meet requirements
+// const fareDoubler = fare => fare*2;
+// const fareTripler=fare=>fare*3;
+
+function selectDifferentDrivers(array,func)
+{
+    return func(array);
+}
 
