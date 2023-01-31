6130e0c03fd97530c0ef6199b617c7c68686b8d8
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-31 10:12:07 -0700

    Creates first four functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=p1=> p1.slice(0,2);
+
+const returnLastTwoDrivers=p1=>p1.slice(2);
+
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num)
+{
+    return (num)=>num*num;
+}
+
+// const fareDoubler = fare =>
+// {
+//     const doubledFare = createFareMultiplier(fare);
+//     return doubledFare; 
+// }
+
+
+
+
